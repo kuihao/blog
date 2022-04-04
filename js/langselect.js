4683a2e8b91b92135f1215d11c634a3b2ce25cff
kuihao <kuihao@users.noreply.github.com>
2022-04-04 07:11:59 +0000

    deploy: 256a40720c0e441451b43153c1bbf56ad3e704da

--- js/langselect.js
+++ js/langselect.js
@@ -1,14 +1,20 @@
 (function() {
   function changeLang() {
+    // Split str of choosed language 
     var selectLang = this.value;
     var canonical = this.dataset.canonical;
-    var url = canonical.split('/')[1]; /* 原本為 canonical.split('/')[0] 
-    但因為 blog 是位於主網止的次層，應抓至取第二節 */
+    var url = canonical.split('/')[0];
     var lang = url === 'index.html' ? '/' : (url === 'en' ? '/' : url);
     var index = canonical.indexOf('/') === -1 ? 0 : canonical.indexOf('/');
     var path = canonical.substring(index);
     path = selectLang + (path === 'index.html' ? '/index.html' : path);
-    location.href = (location.origin + '/' + path).replace('index.html', '');
+    
+    // Split sub directory of `blog`: Kuihao written, my blog is under the main url 
+    let pathname = location.pathname;
+    let subdirectory = pathname.split('/')[1]
+    
+    // Merge url
+    location.href = (location.origin + '/' + subdirectory + '/' + path).replace('index.html', '');
   }
 
   if (document.getElementById('lang-select')) {
