9e017fd1e3b3159db5666ff3e56d56acdcb7830d
Kuihao <kuihaochang@gmail.com>
2022-04-04 14:35:23 +0800

    Changes js of langselect
    The origonal code is split the first slash of url,
    but that would not correct which my blog direct to wrong url.

--- themes/livemylife/source/js/langselect.js
+++ themes/livemylife/source/js/langselect.js
@@ -2,7 +2,8 @@
   function changeLang() {
     var selectLang = this.value;
     var canonical = this.dataset.canonical;
-    var url = canonical.split('/')[0];
+    var url = canonical.split('/')[1]; /* 原本為 canonical.split('/')[0] 
+    但因為 blog 是位於主網止的次層，應抓至取第二節 */
     var lang = url === 'index.html' ? '/' : (url === 'en' ? '/' : url);
     var index = canonical.indexOf('/') === -1 ? 0 : canonical.indexOf('/');
     var path = canonical.substring(index);
